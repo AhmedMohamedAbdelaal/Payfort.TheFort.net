c57851f0f9c781b853584f5c18bc925212ff978a
Vlad Andronache <andronachevgr@gmail.com>
2015-12-06 11:11:55 +0200

    added test for CreateCharge method

--- Start.Net.Tests/ChargeServiceTests.cs
+++ Start.Net.Tests/ChargeServiceTests.cs
@@ -2,23 +2,21 @@
 using Start.Net.Constants;
 using Start.Net.ResponseModels;
 using Start.Net.Entities;
-using NUnit.Framework;
-using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Start.Net.Tests
 {
     [TestClass]
-    [TestFixture]
     public class ChargeServiceTests
     {
         private StartChargeService _service;
-        private CardDetails workingCard; 
+        private CreateChargeRequest _createChargeRequest;
+        private CardDetails _workingCard; 
 
         public ChargeServiceTests()
         {
             _service = new StartChargeService("test_sec_k_63b07e79c620fcfee5a24");
-            workingCard = new CardDetails()
+            _workingCard = new CardDetails()
             {
                 Name = "Abdullah Ahmed",
                 Cvc = 123,
@@ -26,65 +24,94 @@ namespace Start.Net.Tests
                 ExpireYear = 2020,
                 Number = "4242424242424242"
             };
+            _createChargeRequest = new CreateChargeRequest()
+            {
+                Amount = 10000,
+                Currency = Currency.AED,
+                Email = "john.doe@gmail.com"
+            };
         }
 
-        [Test]
         [TestMethod]
-        public void CreateCharge_UsingToken_Success()
+        public void CreateCharge_UsingToken_FailsWithTokenAlreadyUsed()
         {
-            CreateChargeRequest request = new CreateChargeRequest() {
+            _createChargeRequest = new CreateChargeRequest() {
                  Amount = 10000,
                  Currency = Currency.AED,
                 Email = "john.doe@gmail.com"
             };
 
-            request.CardToken = "tok_99af1278b71929fb6c2268fce091";
+            _createChargeRequest.CardToken = "tok_99af1278b71929fb6c2268fce091";
 
-            CreateChargeResponse response = _service.CreateCharge(request).Response;
+            CreateChargeResponse response = _service.CreateCharge(_createChargeRequest).Response;
 
-            NUnit.Framework.Assert.IsFalse(response.IsError);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(response.IsError);
+            Assert.IsTrue(response.IsError);
+            Assert.IsTrue(string.IsNullOrEmpty(response.Id));
+            Assert.IsTrue(response.Error.Type == ErrorType.Request);
         }
 
-        [Test]
         [TestMethod]
         public void CreateCharge_UsingCardDetails_Success()
         {
-            CreateChargeRequest request = new CreateChargeRequest()
-            {
-                Amount = 50000,
-                Currency = Currency.USD,
-                Email = "john.doe@gmail.com"
-            };
+            _createChargeRequest.CardDetails = _workingCard;
 
-            request.CardDetails = workingCard;
+            CreateChargeResponse response = _service.CreateCharge(_createChargeRequest).Response;
 
-            CreateChargeResponse response = _service.CreateCharge(request).Response;
-
-            NUnit.Framework.Assert.IsTrue(!string.IsNullOrEmpty(response.Id));
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(!string.IsNullOrEmpty(response.Id));
+            Assert.IsTrue(!string.IsNullOrEmpty(response.Id));
         }
 
-        [Test]
         [TestMethod]
-        public void CreateCharge_UsingCardDetails_Fails()
+        public void CreateCharge_FailsWithRequestError()
         {
-            CreateChargeRequest request = new CreateChargeRequest()
-            {
-                Amount = 50000,
-                Currency = Currency.USD,
-                Email = "john.doe@gmail.com"
-            };
+            _createChargeRequest.CardDetails = _workingCard;
+            _createChargeRequest.CardDetails.ExpireYear = 2013;
 
-            request.CardDetails = workingCard;
-            request.CardDetails.ExpireYear = 2013;
+            CreateChargeResponse response = _service.CreateCharge(_createChargeRequest).Response;
 
-            CreateChargeResponse response = _service.CreateCharge(request).Response;
+            Assert.IsTrue(response.IsError);
+            Assert.IsTrue(string.IsNullOrEmpty(response.Id));
+            Assert.IsTrue(response.Error.Type == ErrorType.Request);
+        }
 
-            NUnit.Framework.Assert.IsTrue(response.IsError);
-            NUnit.Framework.Assert.IsTrue(string.IsNullOrEmpty(response.Id));
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(response.IsError);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(string.IsNullOrEmpty(response.Id));
+        [TestMethod]
+        public void CreateCharge_FailsWithBankingError()
+        {
+            _createChargeRequest.CardDetails = _workingCard;
+            _createChargeRequest.CardDetails.Number = "4000000000000002";
+
+            CreateChargeResponse response = _service.CreateCharge(_createChargeRequest).Response;
+
+            Assert.IsTrue(response.IsError);
+            Assert.IsTrue(string.IsNullOrEmpty(response.Id));
+            Assert.IsTrue(response.Error.Type == ErrorType.Banking);
+        }
+
+
+        [TestMethod]
+        public void CreateCharge_FailsWithAuthError()
+        {
+            _service = new StartChargeService("wrong_key");
+
+            _createChargeRequest.CardDetails = _workingCard;
+
+            CreateChargeResponse response = _service.CreateCharge(_createChargeRequest).Response;
+
+            Assert.IsTrue(response.IsError);
+            Assert.IsTrue(string.IsNullOrEmpty(response.Id));
+            Assert.IsTrue(response.Error.Type == ErrorType.Authentication);
+        }
+
+        [TestMethod]
+        public void CreateCharge_FailsWith500Error()
+        {
+            _createChargeRequest.CardDetails = _workingCard;
+            _createChargeRequest.CardDetails.Number = "4000000000000127";
+
+            CreateChargeResponse response = _service.CreateCharge(_createChargeRequest).Response;
+
+            Assert.IsTrue(response.IsError);
+            Assert.IsTrue(string.IsNullOrEmpty(response.Id));
+            Assert.IsTrue(response.Error.Type == ErrorType.Processing);
         }
     }
 }
